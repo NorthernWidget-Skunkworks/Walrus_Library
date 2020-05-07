6ae21ef405e1ffb307503dcaa83989885225e880
Andy Wickert <andy@northernwidget.com>
2020-05-06 22:06:18 -0500

    Refactor notes from TPDH to Walrus

--- src/Walrus_I2C.cpp
+++ src/Walrus_I2C.cpp
@@ -1,17 +1,11 @@
 /******************************************************************************
 Walrus.cpp
-Library for TP-Downhole pressure and tempurature sensor, made by Northern Widget LLC.
+Library for Walrus pressure and tempurature sensor, made by Northern Widget LLC.
+Based off of the TP-Downhole
 Bobby Schulz @ Northern Widget LLC
 5/9/2018
-Hardware info located at: https://github.com/NorthernWidget/TP-DownHole
-
-The TP-Downhole is a small form factor tempreture and pressure sensor which uses a combination of an 
-MS5803 pressure sensor and an MCP3421 ADC in combination with a thermistor for high accuracy
-tempreture measurment.
-
-"Size matters not. Look at me. Judge me by my size do you? And well you should not. For my ally is the
-Force, and a powerful ally it is"
--Yoda
+Hardware info located at:
+https://github.com/NorthernWidget-Skunkworks/Project-Walrus
 
 Distributed as-is; no warranty is given.
 ******************************************************************************/
@@ -27,7 +21,7 @@ Walrus::Walrus()
 
 uint8_t Walrus::begin(uint8_t Address_)
 {
-    Wire.begin(); //Initialize Wire instance 
+    Wire.begin(); //Initialize Wire instance
     ADR = Address_; //Copy address to local
 }
 
@@ -38,13 +32,13 @@ float Walrus::getTemperature(uint8_t Location) //Returns temp in C from either s
     int Error = 0; //Error used for testing transmission
     if(Location == 1) Offset = 0x03;
     // Wire.beginTransmission(ADR);
-    // Wire.write(TEMP_REG_0 + Offset); 
+    // Wire.write(TEMP_REG_0 + Offset);
     // Error = Wire.endTransmission();
 
     // Wire.requestFrom(ADR, 3); //Read 3 bytes from device
     for(int i = 0; i < TEMP_OFFSET; i++) { //Increment over read
         Wire.beginTransmission(ADR);
-        Wire.write(TEMP_REG_0 + Offset + i); 
+        Wire.write(TEMP_REG_0 + Offset + i);
         Error = Wire.endTransmission();
         Wire.requestFrom((int)ADR, 1); //Cast ADR to match function
         TempData[i] = Wire.read(); //Read in data
@@ -55,13 +49,13 @@ float Walrus::getTemperature(uint8_t Location) //Returns temp in C from either s
     // Serial.println(TempData[1], HEX);
     // Serial.println(TempData[0], HEX);
     if(Error == 0) return float(long((TempData[2] << 16) | (TempData[1] << 8) | (TempData[0])))/1000.0; //If no error, return concatonated, scaled value
-    else return -9999.0; //Else return error condition 
+    else return -9999.0; //Else return error condition
 }
 
 
 float Walrus::getTemperature() //By default get thermistor temp value
 {
-    return getTemperature(1); 
+    return getTemperature(1);
 }
 
 
@@ -71,27 +65,29 @@ float Walrus::getPressure()
     long TempData[3] = {0}; //Make temp data storage location
     int Error = 0; //Error used for testing transmission
     // Wire.beginTransmission(ADR);  //Command for data
-    // Wire.write(PRES_REG); 
+    // Wire.write(PRES_REG);
     // Error = Wire.endTransmission();
 
     for(int i = 0; i < 3; i++) { //Increment over read
         Wire.beginTransmission(ADR);
-        Wire.write(PRES_REG + i); 
+        Wire.write(PRES_REG + i);
         Error = Wire.endTransmission();
         Wire.requestFrom((int)ADR, 1); //Cast ADR to match function
         TempData[i] = Wire.read(); //Read in data
     }
 
     if(Error == 0) return float(long((TempData[2] << 16) | (TempData[1] << 8) | (TempData[0])))/1000.0; //If no error, return concatonated, scaled value
-    else return -9999.0; //Else return error condition 
+    else return -9999.0; //Else return error condition
 }
 
-String Walrus::GetHeader() 
+String Walrus::GetHeader()
 {
-    return "Pressure [mBar], Temp DH [C], Temp DHt [C], "; //return header string
+    return "Pressure [mBar],Temp DH [C],Temp DHt [C],"; //return header string
 }
 
 String Walrus::GetString()
 {
-    return String(getPressure()) + "," + String(getTemperature(0)) + "," + String(getTemperature(1)) + ","; //Return data string
+    //Return data string
+    return String(getPressure()) + "," + String(getTemperature(0)) + "," \
+                                       + String(getTemperature(1)) + ",";
 }
--- src/Walrus_I2C.h
+++ src/Walrus_I2C.h
@@ -1,17 +1,11 @@
 /******************************************************************************
-TP_Downhole.h
-Library for TP-Downhole pressure and tempurature sensor, made by Northern Widget LLC.
+Walrus.cpp
+Library for Walrus pressure and tempurature sensor, made by Northern Widget LLC.
+Based off of the TP-Downhole
 Bobby Schulz @ Northern Widget LLC
 5/9/2018
-Hardware info located at: https://github.com/NorthernWidget/TP-DownHole
-
-The TP-Downhole is a small form factor tempreture and pressure sensor which uses a combination of an 
-MS5803 pressure sensor and an MCP3421 ADC in combination with a thermistor for high accuracy
-tempreture measurment.
-
-"Size matter not. Look at me. Judge me by my size do you? And well you should not. For my ally is the
-Force, and a powerful ally it is"
--Yoda
+Hardware info located at:
+https://github.com/NorthernWidget-Skunkworks/Project-Walrus
 
 Distributed as-is; no warranty is given.
 ******************************************************************************/
@@ -21,28 +15,80 @@ Distributed as-is; no warranty is given.
 
 #include <Arduino.h>
 
-#define TEMP_REG_0 0x06 //Location of external temp val on Walrus 
+#define TEMP_REG_0 0x06 //Location of external temp val on Walrus
 #define TEMP_REG_1 0x09 //Location of MS5803 temp val on Walrus
 #define TEMP_OFFSET 0x03 //Define length (offset) of tempurate vals in bytes
 #define PRES_REG 0x03 //Location of pressure register on Walrus
 
 #define ADR_DEFAULT 0x0D //Define default address
+
+/**
+ * @class Walrus: .
+ * @brief Class to interface with the Walrus submersible temperature and
+ * presure sensor
+ * @details The Walrus is an encapsulated submersible
+ * pressure and temperature sensor intended for water-level or barometric
+ * monitoring.
+ */
 class Walrus
 {
-public:
-    Walrus();  // Constructor
-    uint8_t begin(uint8_t Address_ = ADR_DEFAULT); // Collect address value, use default
+    public:
 
-    // Return calculated temperature from sensor
-    float getTemperature(uint8_t Location);
-    float getTemperature();
-    // Return calculated pressure from sensor
-    float getPressure();
-    String GetHeader();
-    String GetString();
+        /**
+         * @brief Instantiate Walrus object
+         */
+        Walrus();  // Constructor
 
-private:
-    uint8_t ADR = ADR_DEFAULT; //Default address
+        /**
+         * @brief Begin communications with the Walrus using a prescribed
+         * address.
+         * @param Address_: I2C address of Walrus
+         * DOES NOT YET USE A VARIABLE ADDRESS!
+         * THIS DEFAULT ADDRESS CURRENTLY CLASHES WITH HAAR'S DEFAULT!
+         */
+        uint8_t begin(uint8_t Address_ = ADR_DEFAULT);
+
+        /**
+         * @brief Return calculated temperature from Walrus.
+         * @details This calculated temperature can be from either
+         * the MS5803 sensor, which primarily measures pressure, or from the
+         *
+         * @param Location
+         * TEMP_REG_0: Read dedicated temperature sensor.
+         * TEMP_REG_1: Read temperature sensor withn the MS5803.
+         * APPEARS TO JUST WORK WIHT TEMP_REG_0 FOR NOW
+         */
+        float getTemperature(uint8_t Location);
+
+        /**
+         * @brief Return calculated temperature from sensor, using a predefined
+         * offset.
+         * WHAT DOES THIS OFFSET DO?
+         */
+        float getTemperature();
+
+        /**
+         * @brief Return calculated pressure from sensor.
+         * @details This is the MS5803 sensor, which can come in a variety
+         * of different pressure ranges and sensitivities.
+         */
+        float getPressure();
+
+        /**
+         * @brief Return header
+         * @details "Pressure [mBar],Temp DH [C],Temp DHt [C],"
+         */
+        String GetHeader();
+
+        /**
+         * @brief Return measurement data as a string
+         * @details String(getPressure()) + "," + String(getTemperature(0))
+         + "," + String(getTemperature(1)) + ",";
+         */
+        String GetString();
+
+    private:
+        uint8_t ADR = ADR_DEFAULT; //Default address
 };
 
 #endif
